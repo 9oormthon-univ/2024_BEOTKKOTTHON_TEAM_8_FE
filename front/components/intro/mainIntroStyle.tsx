5888386a3874e02c32187d64d19ef922e3b990ee
leesoyuun <leesoyun4206@naver.com>
2024-03-17 01:39:54 +0900

    Fix: se기종에서 이미지와 텍스트가 겹치는 문제 해결

--- front/components/intro/mainIntroStyle.tsx
+++ front/components/intro/mainIntroStyle.tsx
@@ -8,14 +8,14 @@ const fadeOut = keyframes`
   }
 `;
 export const MainIntroTotal = styled.div`
-  animation: ${fadeOut} 2s ease-out 2s forwards;
+  animation: ${fadeOut} 2s ease-out 1s forwards;
 `;
 export const SubTitle = styled.div`
   font-size: 1.1rem;
-  margin-top: 13.7rem;
   color: #000000;
   opacity: 46%;
   text-align: center;
+  margin-top: 13.7rem;
 `;
 
 export const MainTitle = styled.div`
@@ -28,4 +28,9 @@ export const MainImg = styled.img`
   width: 100%;
   position: absolute;
   bottom: 0;
+  object-fit: cover;
+  @media (max-height: 850px) {
+    position: fixed;
+    bottom: -100px;
+  }
 `;
