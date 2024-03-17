ee6db2855045c673abb9878ac4b41cbd43787f21
leesoyuun <leesoyun4206@naver.com>
2024-03-17 14:49:06 +0900

    Design: 인트로텍스트 위치 수정

--- front/components/intro/textIntro.tsx
+++ front/components/intro/textIntro.tsx
@@ -1,14 +1,30 @@
-import { FirstTxt, SecondTxt, ThirdTxt, FirstSubTxt,SecondSubTxt,ThirdSubTxt } from './textIntroStyle';
+import {
+  First,
+  Second,
+  Third,
+  FirstTxt,
+  SecondTxt,
+  ThirdTxt,
+  FirstSubTxt,
+  SecondSubTxt,
+  ThirdSubTxt,
+} from './textIntroStyle';
 
 const TextIntro = () => {
   return (
     <div>
-      <FirstTxt>{`걱정을 보관하기`}</FirstTxt>
-      <FirstSubTxt>{`지금 떠오르는 걱정이 있으신가요?\n일단 걱정 보관함에 넣어둬요`}</FirstSubTxt>
-      <SecondTxt>{`현실에 집중하기`}</SecondTxt>
-      <SecondSubTxt>{`걱정은 잠시 잊고, 일상에 집중하세요`}</SecondSubTxt>
-      <ThirdTxt>{`걱정과 마주하기`}</ThirdTxt>
-      <ThirdSubTxt>{`걱정 시간이 되면 마음껏 걱정하세요`}</ThirdSubTxt>
+      <First>
+        <FirstTxt>{`걱정을 보관하기`}</FirstTxt>
+        <FirstSubTxt>{`지금 떠오르는 걱정이 있으신가요?\n일단 걱정 보관함에 넣어둬요`}</FirstSubTxt>
+      </First>
+      <Second>
+        <SecondTxt>{`현실에 집중하기`}</SecondTxt>
+        <SecondSubTxt>{`걱정은 잠시 잊고, 일상에 집중하세요`}</SecondSubTxt>
+      </Second>
+      <Third>
+        <ThirdTxt>{`걱정과 마주하기`}</ThirdTxt>
+        <ThirdSubTxt>{`걱정 시간이 되면 마음껏 걱정하세요`}</ThirdSubTxt>
+      </Third>
     </div>
   );
 };
--- front/components/intro/textIntroStyle.tsx
+++ front/components/intro/textIntroStyle.tsx
@@ -16,19 +16,38 @@ const fadeOut = keyframes`
     opacity: 0;
   }
 `;
-
+export const First = styled.div`
+  position: absolute;
+  top: 14.2rem;
+  text-align: left;
+  width: 100%;
+  padding-left: 2.7rem;
+`;
+export const Second = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: 100%;
+`;
+export const Third = styled.div`
+  position: absolute;
+  bottom: 18.7rem;
+  padding-right: 2.7rem;
+  text-align: right;
+  width: 100%;
+`;
 export const FirstTxt = styled.div`
   color: #000;
+  opacity: 70%;
   font-size: 4rem;
-  margin-top: 14.2rem;
-  margin-left: 2.7rem;
   opacity: 0;
   animation:
     ${fadeIn} 2s ease-out forwards,
     ${fadeOut} 1s ease-out 3s forwards;
 `;
 export const FirstSubTxt = styled.div`
-  margin-left: 2.7rem;
   color: #7a7a7a;
   font-size: 2rem;
   white-space: pre-wrap;
@@ -38,40 +57,23 @@ export const FirstSubTxt = styled.div`
     ${fadeIn} 2s ease-out 1s forwards,
     ${fadeOut} 1s ease-out 3s forwards;
 `;
-export const SecondTxt = styled.div`
-  color: #000;
-  opacity: 70%;
-  font-size: 4rem;
-  text-align: center;
-  margin-left: 0rem;
-  opacity: 0;
+export const SecondTxt = styled(FirstTxt)`
   animation:
     ${fadeIn} 2s ease-out 3s forwards,
     ${fadeOut} 1s ease-out 6s forwards;
 `;
-export const SecondSubTxt = styled.div`
-  color: #7a7a7a;
-  font-size: 2rem;
-  text-align: center;
-  opacity: 0;
-  margin-top: 1rem;
-  animation:${fadeIn} 1s ease-out 4s forwards, ${fadeOut} 1s ease-out 6s forwards;
+export const SecondSubTxt = styled(FirstSubTxt)`
+  animation:
+    ${fadeIn} 1s ease-out 4s forwards,
+    ${fadeOut} 1s ease-out 6s forwards;
 `;
-export const ThirdTxt = styled.div`
-  color: #000;
-  opacity: 70%;
-  font-size: 4rem;
-  text-align: right;
-  margin-right: 2.7rem;
-  opacity: 0;
-  animation: ${fadeIn} 2s ease-out 6s forwards, ${fadeOut} 1s ease-out 9s forwards;
+export const ThirdTxt = styled(SecondTxt)`
+  animation:
+    ${fadeIn} 2s ease-out 6s forwards,
+    ${fadeOut} 1s ease-out 9s forwards;
 `;
-export const ThirdSubTxt = styled.div`
-  color: #7a7a7a;
-  font-size: 2rem;
-  text-align: right;
-  margin-right: 2.7rem;
-  margin-top: 1rem;
-  opacity: 0;
-  animation: ${fadeIn} 1s ease-out 7s forwards, ${fadeOut} 1s ease-out 9s forwards;
+export const ThirdSubTxt = styled(SecondSubTxt)`
+  animation:
+    ${fadeIn} 1s ease-out 7s forwards,
+    ${fadeOut} 1s ease-out 9s forwards;
 `;
