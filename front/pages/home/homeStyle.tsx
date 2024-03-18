1269da3dbde112989fac3fc0eb600878161c2c63
leesoyuun <leesoyun4206@naver.com>
2024-03-18 21:06:55 +0900

    Feat: 홈화면 추가

---
+++ front/pages/home/homeStyle.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const SubTitle = styled.div`
+  font-size: 1.2rem;
+  color: rgba(0, 0, 0, 0.5);
+  margin-top: 6rem;
+  margin-bottom: 1rem;
+  text-align: center;
+`;
+export const Time = styled.div`
+  font-size: 3rem;
+  color: rgba(0, 0, 0, 0.5);
+  text-align: center;
+`;
+export const MainImg = styled.img`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+`;
+export const BottomMenues = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 11px;
+  position: absolute;
+  bottom: 23.17vh;
+  width: 100%;
+`;
+export const Menu = styled.div`
+  width: 10.9rem;
+  height: 37px;
+  font-size: 1.6rem;
+  background-color: #d9d9d9;
+  border-radius: 10px;
+  color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const LifeQuotes = styled.div`
+  position: absolute;
+  bottom: 13.6vh;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.5);
+  font-size: 1.2rem;
+  width: 100%;
+`;
--- front/pages/home/index.tsx
+++ front/pages/home/index.tsx
@@ -1,11 +1,28 @@
-import Layout from "@/layout";
-
+import Layout from '@/layout';
+import {
+  SubTitle,
+  Time,
+  MainImg,
+  BottomMenues,
+  Menu,
+  LifeQuotes,
+} from './homeStyle';
 const Home = () => {
-    return (
-        <Layout isHeader={true}>
-            <div></div>
-        </Layout>
-    )
-}
+  return (
+    <Layout isHeader={true}>
+      <SubTitle>{`보관함이 열리기까지`}</SubTitle>
+      <Time>{`1시간 25분 20초`}</Time>
+      <MainImg src="/birdBox.svg" />
+      <BottomMenues>
+        <Menu>{`걱정 넣기`}</Menu>
+        <Menu>{`보관함 열기`}</Menu>
+      </BottomMenues>
+      <LifeQuotes>
+        <div>{`감정은 사실이 아니다. 생각을바꾸면 감정도 바뀐다.`}</div>
+        <div>{`(데이비드 D.번스)`}</div>
+      </LifeQuotes>
+    </Layout>
+  );
+};
 
 export default Home;
