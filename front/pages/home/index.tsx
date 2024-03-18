48b81b80c0cb6e61c4ddc1bbb522b2bdbb5d9841
leesoyuun <leesoyun4206@naver.com>
2024-03-19 01:16:36 +0900

    Style: 스타일컴포넌트 명 보기 깔끔하도록 수정

--- front/pages/home/index.tsx
+++ front/pages/home/index.tsx
@@ -1,26 +1,20 @@
 import Layout from '@/layout';
-import {
-  SubTitle,
-  Time,
-  MainImg,
-  BottomMenues,
-  Menu,
-  LifeQuotes,
-} from './homeStyle';
+import * as h from './homeStyle';
+
 const Home = () => {
   return (
     <Layout isHeader={true}>
-      <SubTitle>{`보관함이 열리기까지`}</SubTitle>
-      <Time>{`1시간 25분 20초`}</Time>
-      <MainImg src="/birdBox.svg" />
-      <BottomMenues>
-        <Menu>{`걱정 넣기`}</Menu>
-        <Menu>{`보관함 열기`}</Menu>
-      </BottomMenues>
-      <LifeQuotes>
+      <h.SubTitle>{`보관함이 열리기까지`}</h.SubTitle>
+      <h.Time>{`1시간 25분 20초`}</h.Time>
+      <h.MainImg src="/birdBox.svg" />
+      <h.BottomMenues>
+        <h.Menu>{`걱정 넣기`}</h.Menu>
+        <h.Menu>{`보관함 열기`}</h.Menu>
+      </h.BottomMenues>
+      <h.LifeQuotes>
         <div>{`감정은 사실이 아니다. 생각을바꾸면 감정도 바뀐다.`}</div>
         <div>{`(데이비드 D.번스)`}</div>
-      </LifeQuotes>
+      </h.LifeQuotes>
     </Layout>
   );
 };
