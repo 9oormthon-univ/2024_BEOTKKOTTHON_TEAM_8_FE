a977983b6a6b4d52497a8b1da1f5ecc1b682be9f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-03-20 03:09:38 +0900

    Feat: 공통으로 쓰일 메신저 컴포넌트 추가

---
+++ front/components/BirdMessenger/index.tsx
@@ -0,0 +1,31 @@
+import { useRecoilValue } from 'recoil';
+import { nameState } from '@/recoil/states';
+import BirdSVG from '../../public/assets/icons/bird.svg';
+import * as S from './styles';
+
+const BirdMessage = ({ isPast }: { isPast: boolean }) => {
+  const name = useRecoilValue(nameState);
+
+  return (
+    <S.Container>
+      <S.ImageWrapper>
+        <BirdSVG />
+      </S.ImageWrapper>
+      <S.Bubble>
+        <S.Message isPast={isPast}>
+          {isPast ? (
+            '도착한 편지가 없어'
+          ) : (
+            <>
+              미래의 {name}에게 <br />
+              따뜻한 마음을 담은 <br />
+              편지를 보내줘
+            </>
+          )}
+        </S.Message>
+        <S.SubMessage>{isPast && '편지가 도착하면 알려줄게'}</S.SubMessage>
+      </S.Bubble>
+    </S.Container>
+  );
+};
+export default BirdMessage;
