81ee640beadb93e8849cb68f0c80f7c13648bc88
leesoyuun <leesoyun4206@naver.com>
2024-03-21 01:05:48 +0900

    Feat: 말풍선 닫기 눌렀을때 없어지는 기능 추가

--- front/components/MainPopup/index.tsx
+++ front/components/MainPopup/index.tsx
@@ -2,11 +2,19 @@ import { useEffect, useState } from 'react';
 import * as S from './styles';
 
 const MainPopup = ({ text, topSize }: { text: string; topSize: number }) => {
+  const [isBubbleVisible, setIsBubbleVisible] = useState(true); // Bubble의 가시성 상태
+
+  const handleCloseClick = () => {
+    setIsBubbleVisible(false); // Close 버튼이 클릭되면 Bubble을 숨김
+  };
+
   return (
-    <S.BubbleContainer topSize={topSize}>
-      <S.Bubble>{text}</S.Bubble>
-      <S.Close src={'/xBtn.svg'} />
-    </S.BubbleContainer>
+    isBubbleVisible && ( // Bubble 가시성 상태에 따라 렌더링 여부 결정
+      <S.BubbleContainer topSize={topSize}>
+        <S.Bubble>{text}</S.Bubble>
+        <S.Close src={'/xBtn.svg'} onClick={handleCloseClick} />
+      </S.BubbleContainer>
+    )
   );
 };
 
