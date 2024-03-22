5398be24a5e74f546c7ed3a117ba96d649e41fd5
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-03-22 18:20:04 +0900

    Fix: svg로 변경

--- front/components/WorryMemoSendNotice/index.tsx
+++ front/components/WorryMemoSendNotice/index.tsx
@@ -1,5 +1,4 @@
 import * as S from './styles';
-import Image from 'next/image';
 
 import EnvelopeSVG from '../../public/assets/icons/envelope.svg';
 import { useRouter } from 'next/router';
@@ -10,7 +9,7 @@ const WorryMemoSendNotice = () => {
   return (
     <S.Container>
       <S.Text>예진의 걱정을 보관할게</S.Text>
-      <Image src={EnvelopeSVG} alt="btn" />
+      <EnvelopeSVG />
       <S.Button onClick={() => router.push('/home')}>돌아가기</S.Button>
     </S.Container>
   );
