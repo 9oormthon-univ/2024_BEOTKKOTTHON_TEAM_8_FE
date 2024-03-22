71ebbd5629391f83abdca7d8edfefc950bdbe6f0
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-03-22 22:57:41 +0900

    Fix: name 가져오기

--- front/components/WorryMemoSendNotice/index.tsx
+++ front/components/WorryMemoSendNotice/index.tsx
@@ -1,15 +1,17 @@
+import { useRecoilValue } from 'recoil';
 import * as S from './styles';
 
-import EnvelopeSVG from '../../public/assets/icons/envelope.svg';
 import { useRouter } from 'next/router';
+import { nameState } from '@/recoil/states';
 
 const WorryMemoSendNotice = () => {
   const router = useRouter();
+  const name = useRecoilValue(nameState);
 
   return (
     <S.Container>
-      <S.Text>예진의 걱정을 보관할게</S.Text>
-      <EnvelopeSVG />
+      <S.Text>{`${name}의 걱정을 보관할게`}</S.Text>
+      <img src="/assets/icons/envelope.svg" />
       <S.Button onClick={() => router.push('/home')}>돌아가기</S.Button>
     </S.Container>
   );
