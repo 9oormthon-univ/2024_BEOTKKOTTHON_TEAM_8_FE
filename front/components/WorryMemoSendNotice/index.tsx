a5e2eac264e45ab0b3592a0e171f9fa33d881fdd
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2024-03-23 21:34:35 +0900

    Feat: frame motion 적용

--- front/components/WorryMemoSendNotice/index.tsx
+++ front/components/WorryMemoSendNotice/index.tsx
@@ -3,17 +3,38 @@ import * as S from './styles';
 
 import { useRouter } from 'next/router';
 import { nameState } from '@/recoil/states';
+import { motion } from 'framer-motion';
+
+const pageVariants = {
+  initial: { opacity: 0.6 },
+  in: { opacity: 1 },
+  out: { opacity: 0.4 },
+};
+
+const pageTransition = {
+  type: 'tween',
+  ease: 'anticipate',
+  duration: 1,
+};
 
 const WorryMemoSendNotice = () => {
   const router = useRouter();
   const name = useRecoilValue(nameState);
 
   return (
-    <S.Container>
-      <S.Text>{`${name}의 걱정을 보관할게`}</S.Text>
-      <img src="/assets/icons/envelope.svg" />
-      <S.Button onClick={() => router.push('/home')}>돌아가기</S.Button>
-    </S.Container>
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransition}
+      style={{ height: '100%' }}>
+      <S.Container>
+        <S.Text>{`${name}의 걱정을 보관할게`}</S.Text>
+        <img src="/assets/icons/envelope.svg" />
+        <S.Button onClick={() => router.push('/home')}>돌아가기</S.Button>
+      </S.Container>
+    </motion.div>
   );
 };
 
--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { AnimatePresence } from 'framer-motion';
 
 import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <AnimatePresence>
+        <Component {...pageProps} />
+      </AnimatePresence>
     </RecoilRoot>
   );
 }
